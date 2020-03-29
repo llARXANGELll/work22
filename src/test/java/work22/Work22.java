550aeef9f0f01a03b35397a933b833722a1c4fdd
llARXANGELll <aramh@inbox.ru>
2020-03-29 20:52:04 +0300

    Сделал

--- src/test/java/work22/Work22.java
+++ src/test/java/work22/Work22.java
@@ -1,9 +1,16 @@
 package work22;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+
 
 public class Work22 {
 
@@ -11,5 +18,10 @@ public class Work22 {
     public void work22() {
 
         open("https://idemo.bspb.ru");
+        $(By.id("login-button")).click();
+        $(By.id("login-otp-button")).click();
+        $(By.id("bank-overview")).click();
+        $(By.xpath("//div[2]/div/div/span/span[normalize-space(@class='amount')]")).shouldHave(text("2 718 764.83 ₽"));
+        $(By.className("my-assets")).waitWhile(text("Моих средств 2 936 972.64 ₽"),50);
     }
 }
