050ecf96de2981bfe1864311837da666c1b0cc22
llARXANGELll <aramh@inbox.ru>
2020-03-29 19:47:11 +0300

    Настроил, начал делать

--- src/test/java/work22/Work22.java
+++ src/test/java/work22/Work22.java
@@ -1,4 +1,15 @@
 package work22;
 
+import com.codeborne.selenide.Selenide;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
 public class Work22 {
+
+    @Test
+    public void work22() {
+
+        open("https://idemo.bspb.ru");
+    }
 }
