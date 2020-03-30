73e7854b4fa33d0fc1d76eb9e22622c0fd0b3e1a
llARXANGELll <aramh@inbox.ru>
2020-03-30 16:35:31 +0300

    Настроил Work23

--- src/test/java/work23/Work23.java
+++ src/test/java/work23/Work23.java
@@ -1,4 +1,22 @@
 package work23;
 
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class Work23 {
+
+    @Test
+    public void work22() {
+
+        open("https://idemo.bspb.ru");
+        $(By.id("login-button")).click();
+        $(By.id("login-otp-button")).click();
+        $(By.id("bank-overview")).click();
+        $(By.xpath("//div[2]/div/div/span/span[normalize-space(@class='amount')]")).shouldHave(text("2 718 764.83 ₽"));
+        $(By.className("my-assets")).waitWhile(text("Моих средств 2 936 972.64 ₽"),50);
+    }
 }
